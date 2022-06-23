0ad5fad055d9011225d3534aefed8cbf6dfce670
Mike Wheeler <mike_wheeler@vfc.com>
2022-06-23 16:28:55 -0700

    Multiple buttons render with imported answers

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import '../styles/questions.scss';
 import Trivia from '../services/Triva';
+import smallBlobYellow from '../images/blob-sm-yellow.png'
+import smallBlobPurple from '../images/blob-sm-purple.png'
+import '../styles/questions.scss';
 
 export default function Questions(props){
 
     return (
         <div className="questions">
             <Trivia/>
+            <img className='questions__blob-yellow' src={smallBlobYellow} alt="small yellow blob" width='162' height='187'/>
+            <img className='questions__blob-purple' src={smallBlobPurple} alt="small purple blob" width='65' height='62'/>
         </div>
     )
 }
--- src/services/Triva.js
+++ src/services/Triva.js
@@ -1,56 +1,78 @@
 import React, {useState, useEffect} from 'react'
-// import axios from 'axios';
-import { nanoid } from 'nanoid';
+import {nanoid} from 'nanoid';
+import he from 'he';
 import '../styles/trivia.scss';
 
 function Trivia() {
-    const [question, setQuestion] = useState([]);
-    const [html, setHtml] = useState([]);
+  const [question, setQuestion] = useState([]);
+  const [html, setHtml] = useState([]);
 
-    function mixQuestionsOrder(correct, incorrect) {
-      const questionArr = [];
-      questionArr.push(correct);
-      incorrect.forEach(element => {
-        questionArr.push(element);
+  function mixQuestionsOrder(correct, incorrect) {
+    const questionArr = [];
+    questionArr.push({
+      answer: correct,
+      selected: false,
+      id: nanoid()
+    });
+    incorrect.forEach(element => {
+      questionArr.push({
+        answer: element,
+        selected: false,
+        id: nanoid()
       });
-      questionArr.sort(()=> Math.random() - 0.5);
-      return questionArr;
-    }
+    });
+    questionArr.sort(() => Math.random() - 0.5);
+    return questionArr;
+  }
 
-    useEffect(()=>{
-      fetch('https://opentdb.com/api.php?amount=5')
+  function button(answers){
+    const answerArr = [];
+    answers.allQuestions.forEach((item)=>{
+      answerArr.push(<button>{he.decode(item.answer)}</button>)
+    })
+    return answerArr
+  }
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5')
       .then(res => res.json())
       .then(question => setQuestion(question.results))
-      .then(()=>{
+      .then(() => {
         setQuestion(prevQuestion => {
           return prevQuestion.map((question => {
             return {
               ...question,
-              allQuestions : mixQuestionsOrder(question.correct_answer, question.incorrect_answers),
-              id: nanoid()
+              allQuestions: mixQuestionsOrder(question.correct_answer, question.incorrect_answers),
+              id: nanoid(),
+              answeredCorrectly: false
             }
           }))
         })
       })
-    }, [])
+  }, [])
 
-    useEffect(()=>{
-      setHtml(() =>{
-        return (
-          question.map((item => {
-            return(
-              <p className="trivia__title" key={item.id}>{item.question}</p>
-            )
-          }))
-        )
-      })
-    }, [question])
+  useEffect(() => {
+    setHtml(() => {
+      return (
+        question.map((item => {
+          return (
+            <div key={item.id}>
+              <p className="trivia__title">{he.decode(item.question)}</p>
+              { button(item) }
+            </div>
+          )
+        }))
+      )
+    });
+    // console.log(JSON.stringify(question, null, 2));
+    // console.log(html);
+
+  }, [question])
 
 
   return (
     <div className="trivia">
       { html }
-      <pre>{JSON.stringify(question, null, 2)}</pre>
     </div>
   )
 }
