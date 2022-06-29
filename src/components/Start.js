f575f6557f1526762e40855b39876c11b5a4800c
Mike Wheeler <mike_wheeler@vfc.com>
2022-06-29 10:31:33 -0700

    Additional text added to start component

--- src/components/Start.js
+++ src/components/Start.js
@@ -11,7 +11,7 @@ export default function Start(props) {
             {props.html.length === 5 ? <button className="start__btn" type="button" onClick={props.startQuiz}><span className="flip-out-hor-top">Loading...</span><span className="flip-in-hor-bottom">Start quiz</span></button> :
                 <button className="start__btn" type="button">Loading...</button>}
             <div className="start__width-full">
-                <p className="start__desc start__challenge">This app was a <a href="https://www.figma.com/file/E9S5iPcm10f0RIHK8mCqKL/Quizzical-App?node-id=0%3A1" target="_blank" rel="noreferrer">Scrimba Challenge</a>. The only direction given was the Figma file itself, the rest was up to the me to code. <a href="https://github.com/MichaelDWheeler/react-quizzical" target="_blank" rel="noreferrer">Source</a>.</p>
+                <p className="start__desc start__challenge">This app was a <a href="https://www.figma.com/file/E9S5iPcm10f0RIHK8mCqKL/Quizzical-App?node-id=0%3A1" target="_blank" rel="noreferrer">Scrimba Challenge</a>. The only direction given was the Figma file itself, the rest was up to the me to code. After checking your answers, you can change your answers to the correct answers. Getting all the answers correct will show a confetti celebration. <a href="https://github.com/MichaelDWheeler/react-quizzical" target="_blank" rel="noreferrer">Source</a>.</p>
             </div>
             <img className="start__blob-yellow" src={blobYellow} alt="yellow blob" />
             <img className="start__blob-purple" src={blobPurple} alt="purple blob" />
